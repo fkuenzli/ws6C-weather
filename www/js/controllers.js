6a6b2bc02849b1852d56dd7eea328b08594e6103
Fabian Künzli <fabian.kuenzli@gmail.com>
2016-04-16 11:45:25 +0200

    add files with new code style

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module( 'starter.controllers', [] )
+angular.module('starter.controllers', [])
 
-  .controller( 'DashCtrl', function ( $scope, $http ) {
+  .controller('DashCtrl', function ($scope, $http) {
 
     var weatherData; // Object
 
@@ -11,19 +11,19 @@ angular.module( 'starter.controllers', [] )
       var lat = 47.3413346;
       var lon = 7.8957823;
       var appid = "3f32ae699559cc963085bac1b8d45a3d";
-      $http.get( url, { params: { "lat": lat, "lon": lon, "units": units, "APPID": appid } } )
-        .success( function ( data ) {
-          console.log( "SUCCESS!" );
+      $http.get(url, {params: {"lat": lat, "lon": lon, "units": units, "APPID": appid}})
+        .success(function (data) {
+          console.log("SUCCESS!");
           weatherData = data;
-        } )
-        .error( function ( data ) {
-          console.log( "ERROR in function getData" );
-        } );
+        })
+        .error(function (data) {
+          console.log("ERROR in function getData");
+        });
 
     }
 
     $scope.parseData = function () {
-      if ( weatherData !== undefined ) {
+      if (weatherData !== undefined) {
         var weatherAsName = weatherData['weather'][0]['main'];
         var temp = weatherData['main']['temp'];
         var location = weatherData['name']
@@ -31,13 +31,13 @@ angular.module( 'starter.controllers', [] )
         console.log(temp + " Grad Celcius");
         console.log("Du bist in " + location);
       } else {
-        console.log( 'Error parsing data!' );
+        console.log('Error parsing data!');
       }
     }
 
-  } )
+  })
 
-  .controller( 'ChatsCtrl', function ( $scope, Chats ) {
+  .controller('ChatsCtrl', function ($scope, Chats) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -47,21 +47,21 @@ angular.module( 'starter.controllers', [] )
     //});
 
     $scope.chats = Chats.all();
-    $scope.remove = function ( chat ) {
-      Chats.remove( chat );
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
     };
-  } )
+  })
 
-  .controller( 'ChatDetailCtrl', function ( $scope, $stateParams, Chats ) {
-    $scope.chat = Chats.get( $stateParams.chatId );
-  } )
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
 
-  .controller( 'AccountCtrl', function ( $scope ) {
+  .controller('AccountCtrl', function ($scope) {
     $scope.settings = {
       enableFriends: true
     };
-  } )
+  })
 
-  .controller( 'RequestCtrl', function ( $scope ) {
-  } );
+  .controller('RequestCtrl', function ($scope) {
+  });
 
